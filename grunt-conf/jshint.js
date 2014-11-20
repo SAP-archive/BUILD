9a8ad7d7336e264ed34fa3ac1f8b32968272ea59
i313281 <tomasz.borychowski@sap.com>
2014-11-20 17:04:11 +0000

    jshintrc tweaks for tets

--- grunt-conf/jshint.js
+++ grunt-conf/jshint.js
@@ -4,17 +4,31 @@ module.exports = {
         jshintrc: 'client/.jshintrc',
         reporter: require('jshint-stylish')
     },
-    dev: [
-        '{client,server}/**/*.js',
-        '!{client,server}/**/*.{spec,mock}.js',
-        '!{client,server}/node_modules/**/*.js'
-    ],
-    tests: [
-        '{client,server}/**/*.{spec,mock}.js',
-        '!{client,server}/node_modules/**/*.js'
-    ],
-    modules: [
-        'node_modules/norman*/**/*.js',
-        '!node_modules/norman*/node_modules/**/*.js',
-    ]
+    dev: {
+        options: {
+            jshintrc: 'client/.jshintrc',
+        },
+        src: [
+            '{client,server}/**/*.js',
+            '!{client,server}/**/*.{spec,mock}.js',
+            '!{client,server}/node_modules/**/*.js',
+
+            'node_modules/norman*/**/*.js',
+            '!node_modules/norman*/**/*.{spec,mock}.js',
+            '!node_modules/norman*/node_modules/**/*.js',
+        ]
+    },
+
+    tests: {
+        options: {
+            jshintrc: 'server/.jshintrc-spec',
+        },
+        src: [
+            '{client,server}/**/*.{spec,mock}.js',
+            '!{client,server}/node_modules/**/*.js',
+
+            'node_modules/norman*/**/*.{spec,mock}.js',
+            '!node_modules/norman*/node_modules/**/*.js'
+        ]
+    }
 };
