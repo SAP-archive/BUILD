cc32b58a59c9d394dceb6289d136c4a4bfba96a7
i313281 <tomasz.borychowski@sap.com>
2014-11-24 15:55:32 +0000

    source maps

--- grunt-conf/less.js
+++ grunt-conf/less.js
@@ -4,6 +4,13 @@ module.exports = {
         paths: [ 'client' ]
     },
     dev: {
+        options: {
+            compress: true,
+            sourceMap: true,
+            sourceMapFilename: 'dev/assets/style.css.map',
+            sourceMapURL: 'style.css.map',
+            // dumpLineNumbers: 'mediaquery'
+        },
         files: {
             'dev/assets/style.css': [
                 'client/*.less',
