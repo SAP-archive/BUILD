d98ff9af81b9a6f6130d8a61a520a9c790ea224f
i313281 <tomasz.borychowski@sap.com>
2014-11-25 16:55:35 +0000

    cleanup

--- client/requires.js
+++ client/requires.js
@@ -3,9 +3,3 @@
 require('norman-auth-client');
 require('norman-shell-client');
 require('./welcome');
-<<<<<<< HEAD
-require('norman-shell-client');
-require('norman-auth-client');
-
-=======
->>>>>>> d4734ff8a8292c2c93976d0d7682f5e0e719b8af
