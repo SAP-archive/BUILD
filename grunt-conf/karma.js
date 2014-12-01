f0b639bb4a0584230a97cf78844f8195d4061c24
David Jones <david.jones02@sap.com>
2014-12-01 11:41:43 +0000

    Fixing karma and jshint to not run node_modules
    Fixing karma and jshint so it doesn't run all of the node_modules tests
    and jshint (this should be done by the modules themselves)

--- grunt-conf/karma.js
+++ grunt-conf/karma.js
@@ -1,6 +1,9 @@
 'use strict';
 module.exports = {
     unit: {
+        options: {
+            files: ['client/**/*spec.js', 'server/**/*spec.js']
+        },
         configFile: 'karma.conf.js',
         singleRun: true
     }
