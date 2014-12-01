2e47e90f6515674221a2cd5882f752a755e92be4
John Long <john.long@sap.com>
2014-12-01 12:53:47 +0000

    Changes to integrate Projects module into Norman.

--- server/requires.js
+++ server/requires.js
@@ -1,6 +1,7 @@
 'use strict';
 // Insert all the requires for optional modules here:
 
-module.exports = function(app) {
-  require('norman-auth-server')(app);
+module.exports = function (app) {
+    require('norman-projects-server')(app);
+    require('norman-auth-server')(app);
 };
