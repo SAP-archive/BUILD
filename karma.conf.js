4cb32d7accee484067388c95586bc47cb67dc2b5
John Maguire <john.maguire@sap.com>
2014-12-02 18:37:45 +0000

    removed 'chai' as sinon-chai includes chai but sinon ignored otherwise

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config) {
     basePath: '',
 
     // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['mocha', 'chai', 'sinon-chai'],
+    frameworks: ['mocha', 'sinon-chai'],
 
     // list of files / patterns to load in the browser
     files: [
