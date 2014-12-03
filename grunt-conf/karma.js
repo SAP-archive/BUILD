f7b842fbe515b8cbc0907f41917f8dfab5615bc5
Tomasz Borychowski <tomasz.borychowski@sap.com>
2014-12-03 10:29:19 +0000

    Update karma.js

--- grunt-conf/karma.js
+++ grunt-conf/karma.js
@@ -1,9 +1,6 @@
 'use strict';
 module.exports = {
     unit: {
-        options: {
-            files: ['client/**/*spec.js', 'server/**/*spec.js']
-        },
         configFile: 'karma.conf.js',
         singleRun: true
     }
