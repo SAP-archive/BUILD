07822b5552ff302e70cb6745ce93d2c51e4b20c9
Tomasz Borychowski <tomasz.borychowski@sap.com>
2015-01-07 16:11:40 +0000

    update norman dependencies & fix deep links

--- client/app.js
+++ client/app.js
@@ -41,13 +41,22 @@ angular.module('norman', modules)
         $urlRouterProvider.otherwise('/');
         $locationProvider.html5Mode(true);
     })
-    .run(function ($rootScope, NavBarService, AsideService) {
+    .run(function ($rootScope, $location, NavBarService, AsideService) {
         $rootScope.navbarService = NavBarService;
         $rootScope.asideService = AsideService;
 
         // add state name as a class to the body
         $rootScope.$on('$stateChangeStart', function (ev, toState) {
+            // add state name to body class
             $rootScope.pageClass = 'page-' + toState.name;
+
+            // redirect (aka deep-link)
+            var path = $location.path().substr(1), redirect = $rootScope.redirect;
+            if (redirect && path !== 'login' && path !== 'signup') {
+                delete $rootScope.redirect;
+                $location.path(redirect);
+            }
         });
+
     })
     .constant('jQuery', require('norman-jquery'));
