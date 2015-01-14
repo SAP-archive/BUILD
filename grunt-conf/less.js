2039e86ef682258b508cce9f2db8e1c040a3e0b2
Tomasz Borychowski <tomasz.borychowski@sap.com>
2015-01-14 16:20:36 +0000

    improve less duplication

--- grunt-conf/less.js
+++ grunt-conf/less.js
@@ -1,24 +1,22 @@
 'use strict';
 module.exports = {
     options: {
-        paths: ['client']
+        paths: ['.', 'client'],
+        compress: false
+        // sourceMap: true,
+        // outputSourceFiles: false,
+        // paths: [ 'less' ],
+        // dumpLineNumbers: 'mediaquery'
+        // sourceMapFilename: 'dev/assets/style.css.map',
+        // sourceMapURL: 'style.css.map'
     },
     dev: {
-        options: {
-            compress: false
-            // sourceMap: true,
-            // outputSourceFiles: false,
-            // paths: [ 'less' ],
-            // dumpLineNumbers: 'mediaquery'
-            // sourceMapFilename: 'dev/assets/style.css.map',
-            // sourceMapURL: 'style.css.map'
-        },
         files: {
             'dev/assets/style.css': [
-                'node_modules/norman-common-client/styles/base.less',
-                'client/**/*.less',
                 'node_modules/norman-*/**/*.less',
                 '!node_modules/norman-common-client/styles/*.less',
+                'node_modules/norman-common-client/styles/base.less',
+                'client/**/*.less',
                 '!node_modules/norman-*/node_modules/**/*.less',
                 '!node_modules/norman-openui5/**/*.less'
             ]
