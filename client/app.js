77ad74d21e555518b5d9f84ef724ccf0a72646a4
i311086 <emanuele.spatola@sap.com>
2015-01-20 16:41:26 +0000

    externalize module requires

--- client/app.js
+++ client/app.js
@@ -1,23 +1,6 @@
 'use strict';
 
-// Require dependencies after angular and before all the aother modules
-require('angular');
-
-var aModule = angular.module, modules = [], dependencies = [];
-angular.module = function (name, dep) {
-    if (dep) {
-        if (name !== 'norman' && dependencies.indexOf(name) === -1) {
-            modules.push(name);
-        }
-        dependencies = dependencies.concat(dep);
-    }
-    return aModule(name, dep);
-};
-
-require('angular-cookies');
-require('angular-resource');
-require('angular-sanitize');
-require('angular-ui-router');
+var modules = require('norman-client-tp');
 
 // Require optional modules
 require('./requires.js');
