ab58c4cb8cf9a9507a60a87da7c81e600db9e446
i053631 <nicolas.lunet@sap.com>
2015-01-28 13:13:57 +0100

    Remove the dependencies to grunt-node-inspector as it is outdated and define a task for ourselves to load a recent version of node-inspector

--- grunt-conf/nodeinspector.js
+++ grunt-conf/nodeinspector.js
@@ -1,19 +1,71 @@
 'use strict';
+/*
+ * grunt-inspector
+ * https://github.com/ChrisWren/grunt-inspector
+ *
+ * Copyright (c) 2013 Chris Wren
+ * Licensed under the MIT license.
+ */
+/*jshint node: true*/
 module.exports = {
+    task: function (grunt) {
+        grunt.registerMultiTask('node-inspector', 'Runs node-inspector to debug your node.js JavaScripts', function () {
+            var options = this.options();
+            var done = this.async();
+            var args = [require.resolve('node-inspector/bin/inspector')];
+            [
+                'web-port',
+                'web-host',
+                'debug-port',
+                'save-live-edit',
+                'readTimeout',
+                'stack-trace-limit',
+                'no-preload',
+                'hidden'
+            ].forEach(function (option) {
+                    if (option in options) {
+                        args.push('--' + option);
+                        if (option === 'hidden') {
+                            args.push(JSON.stringify(options[option]));
+                            return;
+                        }
+                        args.push(options[option]);
+                    }
+                });
 
-    custom: {
-        options: {
-            'web-host': 'localhost',
-            'no-preload': true
-        }
+            grunt.util.spawn({
+                    cmd: 'node',
+                    args: args,
+                    opts: {
+                        stdio: 'inherit'
+                    }
+                },
+                function (error) {
+                    if (error) {
+                        grunt.fail.fatal(error);
+                    }
+                    done();
+                });
+        });
+
     },
 
-    liveEdit: {
-        options: {
-            'save-live-edit': true,
-            'web-host': 'localhost',
-            'no-preload': true
-        }
-    }
+    config: {
 
+        custom: {
+            options: {
+                'web-host': 'localhost',
+                'no-preload': true
+            }
+        },
+
+        liveEdit: {
+            options: {
+                'save-live-edit': true,
+                'web-host': 'localhost',
+                'no-preload': true
+            }
+        }
+
+    }
 };
