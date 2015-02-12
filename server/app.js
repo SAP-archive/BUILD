28d9e9a03bbae394ca46f43babcc690d658bbbe2
Tomasz Borychowski <tomasz.borychowski@sap.com>
2015-02-12 16:27:30 +0000

    admin section updates

--- server/app.js
+++ server/app.js
@@ -5,13 +5,14 @@ var path = require('path');
 var Server = require('norman-app-server').Server;
 var configFile = path.join(__dirname, 'config.json');
 
-var k, n, admin = null;
+var k, n, admin = null, cmd;
 for (k = 2, n = process.argv.length; k < n; ++k) {
     if ((process.argv[k] === '--config') && (k < n - 1)) {
         configFile = process.argv[k + 1];
     }
     if (process.argv[k] === '--create-admin') {
         if (k < n - 2) {
+            cmd = 'create';
             admin = {
                 name: process.argv[k + 1],
                 email: process.argv[k + 2],
@@ -19,22 +20,43 @@ for (k = 2, n = process.argv.length; k < n; ++k) {
             };
         }
         else {
-            console.log('Insufficient create-admin parameters. Please provide name and email.');
+            console.log('Insufficient --create-admin parameters. Please provide name and email.');
+            process.exit(1);
+        }
+    }
+    if (process.argv[k] === '--unassign-admin') {
+        if (k < n - 1) {
+            cmd = 'unassign';
+            admin = { email: process.argv[k + 1] };
+        }
+        else {
+            console.log('Insufficient --unassign-admin parameters. Please provide email.');
             process.exit(1);
         }
     }
 }
 
 Server.start(configFile).then(function () {
+
     // Create Admin user (required for Admin section)
     if (admin) {
-        require('norman-auth-server').createAdmin(admin, function (err) {
-            if (err) {
-                if (err.errors && err.errors.email) err = err.errors.email.message;
-                return console.error('\nERROR: ' + err + '\n');
-            }
+        var auth = require('norman-auth-server'),
+            msg = function (err, success) {
+                if (err) {
+                    if (err.errors && err.errors.email) err = err.errors.email.message;
+                    return console.error('\nERROR: ' + err + '\n');
+                }
+                console.log('\n' + success + '\n');
+            };
 
-            console.log('\nAdmin created!\n');
-        });
+        // create or assign admin role
+        if (cmd === 'create') {
+            auth.createAdmin(admin, function (err, created) {
+                msg(err, created ? 'Admin created!' : 'Admin role assigned to: ' + admin.email);
+            });
+        }
+        else if (cmd === 'unassign') {
+            auth.unassignAdmin(admin, function (err) { msg(err, 'Admin role unassigned!'); });
+        }
     }
 });
