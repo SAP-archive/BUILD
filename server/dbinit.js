a2aeb5aee335cdbf503ada5f570bc836bb9720a7
Sridhar Gandikota <sridhar.gandikota@sap.com>
2015-03-07 08:16:51 +0530

    deploy changes and dbinit changes

--- server/dbinit.js
+++ server/dbinit.js
@@ -1,21 +1,23 @@
 /*eslint no-process-exit: 0*/
 'use strict';
-
+ 
 var path = require('path');
-var Server = require('norman-app-server').Server;
+var AppServer = require('norman-app-server');
 var configFile = path.join(__dirname, 'dbinitconfig.json');
-
-var k, n, admin = null,
-    cmd;
+ 
+var k, n;
 for (k = 2, n = process.argv.length; k < n; ++k) {
     if ((process.argv[k] === '--config') && (k < n - 1)) {
         configFile = process.argv[k + 1];
     }
 }
-
-Server.start(configFile).then(function() {
-
+console.log('server started') 
+var server = new AppServer.Server(configFile);
+server.start().then(function() {
   //create initial data for UI Catalog manager
     var uicatalogMangerServer = require('norman-ui-catalog-manager-server');
-    uicatalogMangerServer.initializeDb();
+    uicatalogMangerServer.initializeDb(function (statuscode) {
+      server.appServer.shutdown(false);
+      console.log(statuscode+'**************');  
+    });
 });
