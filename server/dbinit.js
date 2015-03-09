13677cd5b4dbfc9af3202108b35a395f12e4089a
Sridhar Gandikota <sridhar.gandikota@sap.com>
2015-03-09 14:59:26 +0530

    added error log

--- server/dbinit.js
+++ server/dbinit.js
@@ -1,23 +1,24 @@
 /*eslint no-process-exit: 0*/
 'use strict';
- 
+
 var path = require('path');
 var AppServer = require('norman-app-server');
 var configFile = path.join(__dirname, 'dbinitconfig.json');
- 
+
 var k, n;
 for (k = 2, n = process.argv.length; k < n; ++k) {
     if ((process.argv[k] === '--config') && (k < n - 1)) {
         configFile = process.argv[k + 1];
     }
 }
-console.log('server started') 
 var server = new AppServer.Server(configFile);
 server.start().then(function() {
-  //create initial data for UI Catalog manager
+    //create initial data for UI Catalog manager
     var uicatalogMangerServer = require('norman-ui-catalog-manager-server');
-    uicatalogMangerServer.initializeDb(function (statuscode) {
-      server.appServer.shutdown(false);
-      console.log(statuscode+'**************');  
+    uicatalogMangerServer.initializeDb(function(err) {
+        if (err) {
+            console.log(err);
+        }
+        server.appServer.shutdown();
     });
 });
