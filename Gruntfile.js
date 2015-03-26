58c67fff991cbdaaec8b5dc50f57353c2d33f2d2
nluo <ningping.luo@sap.com>
2015-03-26 13:11:39 +0100

    #431 add bump publish grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var path = require('path');
 var nodeInspector = require('./grunt-conf/nodeinspector.js');
 
+var projects = [
+];
 var repo = "Norman";
 var settings = require("./config.json");
 
