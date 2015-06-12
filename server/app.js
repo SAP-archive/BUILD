73b3a9d38ec136f1a2bc5decc2757284de7f328d
i051710 <jerome.leconte@sap.com>
2015-06-12 10:07:08 +0200

    create Norman's adim. Use the createAdmin script on admin app

--- server/app.js
+++ server/app.js
@@ -5,55 +5,20 @@ var path = require('path');
 var AppServer = require('node-sap-app-server');
 var configFile = path.join(__dirname, 'config.json');
 
-var k, n, admin = null, cmd;
+var k, n;
 for (k = 2, n = process.argv.length; k < n; ++k) {
     if ((process.argv[k] === '--config') && (k < n - 1)) {
         configFile = process.argv[k + 1];
     }
     if (process.argv[k] === '--create-admin') {
-        if (k < n - 2) {
-            cmd = 'create';
-            admin = {
-                name: process.argv[k + 1],
-                email: process.argv[k + 2],
-                password: process.argv[k + 3] || null
-            };
-        }
-        else {
-            console.log('Insufficient --create-admin parameters. Please provide name and email.');
-            process.exit(1);
-        }
+        console.warn('Deprecated create-admin. Use the createAdmin script on admin app.');
+        process.exit(1);
     }
     if (process.argv[k] === '--unassign-admin') {
-        if (k < n - 1) {
-            cmd = 'unassign';
-            admin = { email: process.argv[k + 1] };
-        }
-        else {
-            console.log('Insufficient --unassign-admin parameters. Please provide email.');
-            process.exit(1);
-        }
+        console.warn('Deprecated unassign admin. Use the createAdmin script on admin app.');
+        process.exit(1);
     }
 }
 
 var server = new AppServer.Server(configFile);
-server.start()
-    .then(function () {
-        // Create Admin user (required for Admin section)
-        switch (cmd) {
-            case 'create':
-            case 'unassign':
-                var registry = require('norman-common-server').registry,
-                    aclService = registry.getModule('AclService');
-
-                if (!aclService) return console.error('ACL service not found. Please update norman-auth-server');
-
-                // create, assign or unassign admin role
-                aclService[cmd + 'Admin'](admin).then(function () {
-                    console.log('\nAdmin role ' + cmd + 'ed, ' + admin.email + '\n');
-                });
-                break;
-            default:
-                break;
-        }
-    });
+server.start();
