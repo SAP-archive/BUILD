bb74bda28f9a87117c5cc1227b46b5777a0a77e7
Ian Quigley <ian.quigley@sap.com>
2015-06-22 23:46:47 +0100

    Removing norman-common-client references

--- grunt-conf/less.js
+++ grunt-conf/less.js
@@ -14,10 +14,12 @@ module.exports = {
         files: {
             'dev/assets/style.css': [
                 'node_modules/norman-*/**/*.less',
-                '!node_modules/norman-common-client/styles/*.less',
-                'node_modules/norman-common-client/styles/base.less',
+                '!node_modules/norman-client-tp/node_modules/angular-sap-*/styles/*.less',
+                'node_modules/norman-client-tp/node_modules/angular-sap-ui-elements/styles/base.less',
                 'client/**/*.less',
                 '!node_modules/norman-*/node_modules/**/*.less',
+                'node_modules/norman-client-tp/node_modules/angular-sap-*/**/*.less',
+                '!node_modules/norman-client-tp/node_modules/angular-sap-ui-elements/node_modules/**/*.less',
                 '!node_modules/norman-openui5/**/*.less'
             ]
         }
